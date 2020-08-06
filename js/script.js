4838493086d5a182186e697f7853c18bc316e749
Andrey <petukhovanvk@gmail.com>
2020-08-06 11:47:23 +0300

    переработано с использованием регулярных выражений. создана ф-ция deleteotigits

--- js/script.js
+++ js/script.js
@@ -469,11 +469,15 @@ showTabContent();
         dots.push(dot);
     }
 
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
-        if( offset == +width.slice(0, width.length - 2) * (slides.length - 1)){
+        if( offset == deleteNotDigits(width) * (slides.length - 1)){
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -496,9 +500,9 @@ showTabContent();
 
     prev.addEventListener('click', () => {
         if(offset == 0){
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);            
+            offset = deleteNotDigits(width) * (slides.length - 1);            
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -524,7 +528,7 @@ showTabContent();
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = deleteNotDigits(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
